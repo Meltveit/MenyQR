d5e055b6987cdbaf08155eb00a28e181a87fa065
Christopher Nielsen <Kontakt@fm-service.no>
2025-11-16 18:36:56 +0000

    ./src/hooks/use-auth.tsx:11:1 Module not found: Can't resolve '@/lib/fir

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+
+export { app, auth };
